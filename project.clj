3ebc7b71843a9736d704fc97821df26e35a295ce
lwg <lwg@lwg-PC>
2016-06-19 17:54:34 +0800

    httpkit test

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
-(defproject virgo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+(defproject virgo "1.0.0-SNAPSHOT" 
+  :description "A sample project"
+  
+  :url "http://example.org/sample-clojure-project"  
+
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [mysql/mysql-connector-java "5.1.25"]
+                 [korma "0.3.0"]
+                 [http-kit "2.1.16"]]
+  :repositories {"java.net" "http://download.java.net/maven/2"
+                 "mvn" "http://mvnrepository.com/"
+                 "repo1" "http://repo1.maven.org/maven2/"
+                 })
+
+
